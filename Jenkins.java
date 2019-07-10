a1a3d21b317bd21f059678abb86538dbdf78bb6a
niazhaque <md.niazul@gmail.com>
2019-07-10 15:52:04 -0400

    Update Jenkins.java
    adding notes

--- Jenkins.java
+++ Jenkins.java
@@ -1,5 +1,6 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ * Just a test 
  */
 public class HelloWorld {
   public static void main(String[] args) {
